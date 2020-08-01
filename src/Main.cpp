31383d08d8fb2e136cc722234ba43d036ea1cae1
Shunsei <khan.sufyan.cs@gmail.com>
2020-08-01 17:17:35 -0400

    Draws a rectangle to the screen

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,7 @@
 #include "Graphics/Screen.h"
 #include "Shapes/Line2D.h"
 #include "Shapes/Triangle.h"
+#include "Shapes/AARectangle.h"
 
 const int SCREEN_WIDTH = 224;
 const int SCREEN_HEIGHT = 288;
@@ -15,15 +16,21 @@ int main(int argc, char* argv[]) {
 	theScreen.Init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION);
 	
 	/*
-	Line2D line = {Vec2D(0,0), Vec2D(SCREEN_WIDTH, SCREEN_HEIGHT)};
+	Line2D line{Vec2D(0,0), Vec2D(SCREEN_WIDTH, SCREEN_HEIGHT)};
 	theScreen.Draw(line, Color::Yellow());
 	theScreen.SwapScreens();	
 	*/
 	
-	Triangle triangle = {Vec2D(SCREEN_WIDTH / 2, 10), Vec2D(10, SCREEN_HEIGHT / 2), Vec2D(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)};
+	/*
+	Triangle triangle{Vec2D(SCREEN_WIDTH / 2, 10), Vec2D(10, SCREEN_HEIGHT / 2), Vec2D(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)};
 	theScreen.Draw(triangle, Color::Blue());
 	theScreen.SwapScreens();	
+	*/
 
+	AARectangle rect{Vec2D(20, 50), 50, 100};
+	theScreen.Draw(rect, Color::Red());
+	theScreen.SwapScreens();	
+	
 	SDL_Event sdlEvent;
 	bool running = true;
 
