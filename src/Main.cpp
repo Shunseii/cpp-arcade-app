4010baea9f793a75e42a61790d20136396fa9121
Shunsei <khan.sufyan.cs@gmail.com>
2020-08-01 16:54:36 -0400

    Draws triangle to screen

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,6 +4,7 @@
 #include "Graphics/Color.h"
 #include "Graphics/Screen.h"
 #include "Shapes/Line2D.h"
+#include "Shapes/Triangle.h"
 
 const int SCREEN_WIDTH = 224;
 const int SCREEN_HEIGHT = 288;
@@ -13,8 +14,14 @@ int main(int argc, char* argv[]) {
 	Screen theScreen;
 	theScreen.Init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION);
 	
+	/*
 	Line2D line = {Vec2D(0,0), Vec2D(SCREEN_WIDTH, SCREEN_HEIGHT)};
 	theScreen.Draw(line, Color::Yellow());
+	theScreen.SwapScreens();	
+	*/
+	
+	Triangle triangle = {Vec2D(SCREEN_WIDTH / 2, 10), Vec2D(10, SCREEN_HEIGHT / 2), Vec2D(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)};
+	theScreen.Draw(triangle, Color::Blue());
 	theScreen.SwapScreens();	
 
 	SDL_Event sdlEvent;
