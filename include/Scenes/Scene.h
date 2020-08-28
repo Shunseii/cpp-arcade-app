aeb05ae3281e0a3c5d40f1c09f3fc4973dc9ad58
Shunsei <khan.sufyan.cs@gmail.com>
2020-08-28 06:35:53 -0400

    Adds GameController member

--- include/Scenes/Scene.h
+++ include/Scenes/Scene.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <stdint.h>
 
+#include "Input/GameController.h"
+
 class Screen;
 
 // Scene Interface
@@ -14,6 +16,11 @@ class Scene {
 		virtual void Update(uint32_t dt) = 0;
 		virtual void Draw(Screen&) = 0;
 		virtual const std::string& GetSceneName() const = 0; 
+		
+		inline GameController* GetGameController() {return &mGameController;}
+
+	protected:
+		GameController mGameController;
 };
 
 #endif
